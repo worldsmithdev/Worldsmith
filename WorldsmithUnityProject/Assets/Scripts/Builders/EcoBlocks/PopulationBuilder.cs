4e9479ee6cc85626c0d6f62322d9b16923b30278
tmo <worldsmithdev@gmail.com>
2019-05-08 16:50:50 +0200

    Separate Economy Builders

---
+++ WorldsmithUnityProject/Assets/Scripts/Builders/EcoBlocks/PopulationBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationBuilder : MonoBehaviour
+{
+    public void BuildPopulations()
+    {
+        foreach (Location loc in WorldController.Instance.GetWorld().locationList)
+            if (loc.GetLocationType() == Location.LocationType.Settled)
+                if (loc.GetLocationSubType() != Location.LocationSubType.Homestead && loc.GetLocationSubType() != Location.LocationSubType.Dwelling)
+                    BuildSettledPopulations(loc);
+    }
+
+
+
+
+    void BuildSettledPopulations(Location loc)
+    {
+        Population pop = new Population(loc);
+        EconomyController.Instance.populationDictionary.Add(pop, null);
+    }
+}
---
+++ WorldsmithUnityProject/Assets/Scripts/Builders/EcoBlocks/WarbandBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarbandBuilder : MonoBehaviour
+{
+    public void BuildWarbands()
+    {
+        foreach (Location loc in WorldController.Instance.GetWorld().locationList)
+            if (loc.GetLocationType() == Location.LocationType.Settled)
+                if (loc.GetLocationSubType() != Location.LocationSubType.Homestead && loc.GetLocationSubType() != Location.LocationSubType.Dwelling)
+                    BuildSettledWarbands(loc);
+    }
+
+    void BuildSettledWarbands(Location loc)
+    {
+        Warband warband = new Warband(loc);
+        EconomyController.Instance.warbandDictionary.Add(warband, null);
+    }
+
+}
